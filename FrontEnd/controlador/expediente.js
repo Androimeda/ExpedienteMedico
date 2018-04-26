ba4be3e1583042aa0143c50f60d367cf189d46f8
Antonio Aguilar <jaguilar@gmail.com>
2018-04-26 13:04:36 -0600

    Cross Origin

--- FrontEnd/controlador/expediente.js
+++ FrontEnd/controlador/expediente.js
@@ -1,1 +1,22 @@
 $("#nav-item-paciente").addClass("active");
+
+$(document).ready(function() {
+  $.ajax({
+    url: CONST_SITIO_URL + '/services/paciente.php',
+    method: 'POST',
+    dataType: 'JSON',
+    crossDomain: true,
+    data: {
+      'accion': 'listarTodos',
+    },
+    success: function (respuesta) {
+      console.log(respuesta);
+    },
+    error: function (error) {
+      console.log(error);
+    },
+    complete: function () {
+      //TO-DO
+    }
+  });
+});
---
+++ FrontEnd/js/config.js
@@ -0,0 +1,1 @@
+const CONST_SITIO_URL = 'http://192.168.1.13/syme/Backend'
