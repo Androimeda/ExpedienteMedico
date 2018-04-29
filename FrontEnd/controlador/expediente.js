4da61bdedfa2f75def79fcc3b8a02cfa8248dd11
Jose Antonio Aguilar <jaguilar992@gmail.com>
2018-04-29 10:56:10 -0600

    Funciones

--- FrontEnd/controlador/expediente.js
+++ FrontEnd/controlador/expediente.js
@@ -11,7 +11,7 @@ function buscar(){
       'noIdentidad': valor,
     },
     success:function(respuesta){
-      console.log(respuesta);
+      agregarFilaTablaBusqueda(respuesta);
     },
     error: function(error){
       console.log(error);
@@ -20,4 +20,96 @@ function buscar(){
       //TO-DO
     }
   });
+}
+
+function agregarFilaTablaBusqueda(respuesta){
+  $("#tbl-resultado tbody").empty();
+  for (var i = 0; i < respuesta.length; i++) {
+    var paciente = respuesta[i];
+    var fila = 
+    "<tr>"+
+    "  <td>"+paciente.ID_EXPEDIENTE+"</td>"+
+    "  <td>"+paciente.P_NOMBRE+" "+paciente.S_NOMBRE+" "+paciente.P_APELLIDO+" "+paciente.S_APELLIDO+"</td>"+
+    "  <td>"+paciente.NO_IDENTIDAD+"</td>"+
+    '  <td>'+
+      '<button onclick="expediente('+paciente.ID_EXPEDIENTE+')" class="btn btn-default btn-sm">'+
+        '<span class="glyphicon glyphicon-eye-open"></span>'+
+      '</button>'+
+    "</td>"+
+    "</tr>";
+    $("#tbl-resultado tbody").append(fila);
+  }
+}
+
+
+function limpiar(){
+  $('#tbl-resultado tbody').empty();
+  $('#txt-noidentidad').val("");
+}
+
+function expediente(id){
+  $(".sector-exp").show();
+  $.ajax({
+    url:CONST_SITIO_URL+'/services/ConsultaExterna.php',
+    method:'POST',
+    dataType:'JSON',
+    data:{
+      'accion':'listarPorPaciente',
+      'idExpediente': id,
+    },
+    success:function(respuesta){
+      $("#tbl-consulta tbody").empty();
+      for (var i = 0; i < respuesta.length; i++) {
+        consulta = respuesta[i];
+        var fila =
+        "<tr>"+
+        "  <td>"+consulta.FECHA_HORA+"</td>"+
+        "  <td>"+consulta.CENTRO_MEDICO+"</td>"+
+        "  <td>"+consulta.PISO+"</td>"+
+        "  <td>"+consulta.MEDICO+"</td>"+
+        "  <td>"+consulta.ESPECIALIDAD+"</td>"+
+        "  <td>"+consulta.SINTOMAS+"</td>"+
+        "  <td>"+consulta.DIAGNOSTICO+"</td>"+
+        "</tr>";
+        $("#tbl-consulta tbody").append(fila);
+      }
+    },
+    error: function(error){
+      console.log(error);
+    },
+    complete: function(){
+      //TO-DO
+    }
+  });
+
+  $.ajax({
+  url:CONST_SITIO_URL+'/services/Emergencia.php',
+  method:'POST',
+  dataType:'JSON',
+  data:{
+    'accion':'listarPorPaciente',
+    'idExpediente': id,
+  },
+  success:function(respuesta){
+    $("#tbl-emergencia tbody").empty();
+    for (var i = 0; i < respuesta.length; i++) {
+      emergencia = respuesta[i];
+      var fila =
+      "<tr>"+
+      "  <td>"+emergencia.FECHA_HORA_ATENCION+"</td>"+
+      "  <td>"+emergencia.CENTRO_MEDICO+"</td>"+
+      "  <td>"+emergencia.MEDICO+"</td>"+
+      "  <td>"+emergencia.ESPECIALIDAD+"</td>"+
+      "  <td>"+emergencia.OBSERVACION+"</td>"+
+      "</tr>";
+      $("#tbl-emergencia tbody").append(fila);
+    }
+  },
+  error: function(error){
+    console.log(error);
+  },
+  complete: function(){
+    //TO-DO
+  }
+});
 }
--- FrontEnd/expediente.php
+++ FrontEnd/expediente.php
@@ -16,24 +16,84 @@
     <!-- Fin Navbar -->
     <div class="container">   
         <div class="row">
-            <div class="col-lg-4 well card">
-                <h5>Buscar Expediente</h5>
+            <div class="col-lg-8 well card">
+                <h4>Buscar Expediente</h4>
                 <table class="table">
                     <tr>
                         <td>No de indentidad</td>
                         <td><input type="text" placeholder="Ingresar" class="form-control" id="txt-noidentidad"></td>
                     </tr>
                     <tr>
-                        <td colspan="2"><button type="button" class="btn btn-primary" onclick="buscar()">Buscar</button></td>
+                        <td colspan="2">
+                            <button type="button" class="btn btn-default" onclick="limpiar()">Limpiar</button>
+                            <button type="button" class="btn btn-primary" onclick="buscar()">Buscar</button>
+                        </td>
                     </tr>
                 </table>
+
+                <table class="table" id="tbl-resultado">
+                    <thead>
+                        <th>Expediente</th>
+                        <th>Nombre</th>
+                        <th>No Identidad</th>
+                        <th>Ver Expediente</th>
+                    </thead>
+                    <tbody></tbody>
+                </table>
             </div>
-            <div class="col-lg-8 well card">
-            <h5>Expediente</h5>
-                
+            <div class="col-lg-12 well card sector-exp">
+            <h4>Expediente</h4>
+            <h5>Consulta Externa</h5>
+                <div class="row">
+                  <div class="col-lg-12">
+                    <table class="table" id="tbl-consulta">
+                      <thead>
+                        <th>Fecha</th>
+                        <th>Centro Medico</th>
+                        <th>Piso</th>
+                        <th>Medico</th>
+                        <th>Especialidad</th>
+                        <th>Síntomas</th>
+                        <th>Diagnostico</th>
+                      </thead>
+                      <tbody>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+            </div>            
+            <div class="col-lg-12 well card sector-exp">
+            <h5>Emergencia</h5>
+                <div class="row">
+                  <div class="col-lg-12">
+                    <table class="table" id="tbl-emergencia">
+                      <thead>
+                        <th>Fecha</th>
+                        <th>Centro Medico</th>
+                        <th>Medico</th>
+                        <th>Especialidad</th>
+                        <th>Observacion</th>
+                      </thead>
+                      <tbody>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
             </div>
         </div>
     </div> <!-- /container -->
+    <!-- FAB ICON -->
+    <div class="container fab">
+      <div class="row">
+        <div class="col-lg-11 col-lg-offsset-11"></div>
+        <div class="col-lg-1">
+          <a href="#" class="btn btn-success"><span class="glyphicon glyphicon-home"></span></a>
+          <a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-chevron-up"></span></a>
+        </div>
+      </div>
+    </div>
+    <!-- /FAB ICON -->
+
     <script src="./js/jquery.min.js"></script>
     <script src="./js/bootstrap.min.js"></script>
 
