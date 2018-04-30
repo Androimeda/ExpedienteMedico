5f95937d3d2debbb1ffc4a4b332a06bd320d8ea9
Androimeda <mnunezo@unah.hn>
2018-04-30 10:15:30 -0600

    pantallas-APH-EMERGENCIA

--- FrontEnd/atencion-fecha.php
+++ FrontEnd/atencion-fecha.php
@@ -14,7 +14,7 @@
     <!-- Fixed navbar -->
     <?php include("nav-bar.php") ?>
     <!-- Fin Navbar -->
-    <div class="container"> 
+    <div class="container well card"> 
     <div class="row">
         <div class="col-lg-12" id="titulo">
           <h1><?php echo $user["CENTRO_MEDICO"] ?></h1>
@@ -44,6 +44,7 @@
         <div class="col-lg-12">
           <table class="table table-striped" id="tbl-aph">
             <thead>
+              <th>Fecha</th>
               <th>Atencion</th>
               <th>Placa</th>
               <th>id Centro</th>
@@ -63,7 +64,7 @@
       </div>
       <div class="row">
         <div class="col-lg-12">
-          <a href="agregar-paciente.php" class="btn btn-primary">Agrega Paciente <span class="glyphicon glyphicon-plus"></span></a>
+          <a href="agregar-paciente.php" class="btn btn-primary">Agrega Atencion PreHospitalaria <span class="glyphicon glyphicon-plus"></span></a>
         </div>
       </div>
     </div> <!-- /container -->
