56452356ff4650e2a7b701ffd377fef7b479de3a
Androimeda <mnunezo@unah.hn>
2018-04-30 11:53:27 -0600

    aph

--- FrontEnd/controlador/atencion-hoy.js
+++ FrontEnd/controlador/atencion-hoy.js
@@ -1,1 +1,47 @@
 $("#nav-item-emergencia").addClass("active")
+
+
+function agregarFilaTablaAPHoy(respuesta){
+	$("#tbl-aph tbody").empty();
+	for (var i = 0; i < respuesta.length; i++) {
+		var aph = respuesta[i];
+		var fila = 
+		"<tr>"+
+		"  <td>"+aph.FECHA_HORA_ATENCION+"</td>"+
+		"  <td>"+aph.ID_ATENCION+"</td>"+
+		"  <td>"+aph.PLACA+"</td>"+
+		"  <td>"+aph.ID_CENTRO+"</td>"+
+		"  <td>"+aph.OBSERVACION+"</td>"+
+		"  <td>"+aph.P_NOMBRE+"</td>"+
+		"  <td>"+aph.S_NOMBRE+"</td>"+
+		"  <td>"+aph.P_APELLIDO+"</td>"+
+		"  <td>"+aph.S_APELLIDO+"</td>"+
+		"  <td>"+aph.NO_IDENTIDAD+"</td>"+
+		"  <td>"+aph.SEXO+"</td>"+
+		"  <td>"+aph.ID_PARAMEDICO+"</td>"+
+		'  	<td><button onclick="editar('+aph.ID_ATENCION+')" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>'+
+		"</tr>"
+		$("#tbl-paciente tbody").append(fila);
+	}
+}
+
+function cargaTablaAPHoy(){
+	$.ajax({
+	  url:CONST_SITIO_URL+'/services/AtencionPreHospitalaria.php',
+	  method:'POST',
+	  dataType:'JSON',
+	  data:{
+	    'accion':'listarPorCentroFecha',
+	    
+	  },
+	  success:function(respuesta){
+	  	agregarFilaTablaAPHoy(respuesta);
+	  },
+	  error: function(error){
+	    console.log(error);
+	  },
+	  complete: function(){
+	    //TO-DO
+	  }
+	});
+}
