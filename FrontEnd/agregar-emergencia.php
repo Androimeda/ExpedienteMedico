e0912c1fb54d3c4432d5c114bcc45df900721319
Androimeda <mnunezo@unah.hn>
2018-05-01 09:20:08 -0600

    agregar-emergencia

--- FrontEnd/agregar-emergencia.php
+++ FrontEnd/agregar-emergencia.php
@@ -14,8 +14,111 @@
     <!-- Fixed navbar -->
     <?php include("nav-bar.php") ?>
     <!-- Fin Navbar -->
-    <div class="container">      
+     <div class="container">   
+        <div class="row well card">
+            <div class="col-lg-12" id="titulo">
+                <h1><?php echo $user["CENTRO_MEDICO"] ?></h1>
+                <h1>Registro de Emergencias (Ingreso)</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="col-lg-12 well card">
+                    <h4>Buscar Paciente</h4>
+                    <table class="table" id="tbl-busqueda">
+                        <tr>
+                            <td>No de indentidad</td>
+                            <td><input type="text" onkeyup="buscar()" placeholder="Ingresar" class="form-control" id="txt-noidentidad"></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <button type="button" class="btn btn-default" onclick="limpiar()">Limpiar</button>
+                                <button type="button" class="btn btn-primary" onclick="buscar()">Buscar</button>
+                            </td>
+                        </tr>
+                    </table>
+
+                    <table class="table" id="tbl-resultado">
+                        <thead>
+                            <th>Expediente</th>
+                            <th>Nombre</th>
+                            <th>No Identidad</th>
+                            <th>Seleccionar</th>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+                <div class="col-lg-12 well card">
+                  <h4>Buscar Médico</h4>
+                  <table class="table" id="tbl-busqueda-medico">
+                    <tr>
+                      <td>No de indentidad</td>
+                      <td><input type="text" placeholder="Ingresar" class="form-control" id="txt-noidentidad-medico"></td>
+                    </tr>
+                    <tr>
+                      <td colspan="2">
+                        <button type="button" class="btn btn-default" onclick="limpiarTablaMedico()">Limpiar</button>
+                        <button type="button" class="btn btn-primary" onclick="buscarMedico()">Buscar</button>
+                      </td>
+                    </tr>
+                  </table>
+
+                  <table class="table" id="tbl-resultado-medico">
+                    <thead>
+                      <th>No Colegiacion</th>
+                      <th>Nombre</th>
+                      <th>No Identidad</th>
+                      <th>Seleccionar</th>
+                    </thead>
+                    <tbody></tbody>
+                  </table>
+                </div>
+            </div>
+            <div class="col-lg-6 well card">
+                <h4>Datos de Ingreso:</h4>
+                <table class="table">
+                    <tr>
+                        <td>Expediente</td>
+                        <td colspan="2"><input type="text" disabled="disabled" class="form-control" id="txt-id-expediente"></td>
+                    </tr>
+                    <tr>
+                      <td>Medico (Identificador)</td>
+                      <td colspan="2"><input type="text" disabled="disabled" class="form-control" id="txt-id-medico"></td>
+                    </tr>
+                    <tr>
+                        <td>Observación:</td>
+                        <td colspan="2">
+                            <textarea id="txt-observacion" class="form-control"></textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                    <tr>
+                        <td>Atencion:</td>
+                        <td colspan="2">
+                            <input id="txt-atencion" type="text" class="form-control">
+                        </td>
+                    </tr>
+                    <tr>  <tr>
+                        <td>Centro Medico:</td>
+                        <td colspan="2">
+                            <input id="txt-centroMedico" type="text" class="form-control">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Fecha/Hora</td>
+                        <td><input type="date" id="txt-fecha" class="form-control"></td>
+                        <td><input type="time" id="txt-hora" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">
+                            <button type="button" class="btn btn-primary btn-block" onclick="registrar()">Registrar</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>   
     </div> <!-- /container -->
+    <<input type="hidden" name="" value="">
     <script src="./js/jquery.min.js"></script>
     <script src="./js/bootstrap.min.js"></script>
 
--- FrontEnd/controlador/agregar-emergencia.js
+++ FrontEnd/controlador/agregar-emergencia.js
@@ -1,1 +1,146 @@
 $("#nav-item-emergencia").addClass("active");
+
+var paciente = null;
+var medico = null;
+
+function limpiar(){
+  $('#tbl-resultado tbody').empty();
+  $('#txt-noidentidad').val("");
+}
+
+function buscar(){
+  var valor = $("#txt-noidentidad").val();
+  $.ajax({
+    url:CONST_SITIO_URL+'/services/Paciente.php',
+    method:'POST',
+    dataType:'JSON',
+    data:{
+      'accion':'buscarPorNoIdentidad',
+      'noIdentidad': valor,
+    },
+    success:function(respuesta){
+      agregarFilaTablaBusqueda(respuesta);
+      paciente = respuesta;
+    },
+    error: function(error){
+      console.log(error);
+    },
+    complete: function(){
+      //TO-DO
+    }
+  });
+}
+
+function agregarFilaTablaBusqueda(respuesta){
+  $("#tbl-resultado tbody").empty();
+  for (var i = 0; i < respuesta.length; i++) {
+    var paciente = respuesta[i];
+    var fila = 
+    "<tr>"+
+    "  <td>"+paciente.ID_EXPEDIENTE+"</td>"+
+    "  <td>"+paciente.P_NOMBRE+" "+paciente.S_NOMBRE+" "+paciente.P_APELLIDO+" "+paciente.S_APELLIDO+"</td>"+
+    "  <td>"+paciente.NO_IDENTIDAD+"</td>"+
+    '  <td>'+
+        '<button onclick="seleccionar('+i+')" class="btn btn-default btn-md">'+
+          '<span class="glyphicon glyphicon-ok"></span> &nbsp; Seleccionar'+
+        '</button>'+
+      "</td>"+
+    "</tr>";
+    $("#tbl-resultado tbody").append(fila);
+  }
+}
+
+function seleccionar(i){
+	limpiar();
+	var p = paciente[i];
+	$("#txt-id-expediente").val(p.ID_EXPEDIENTE);
+}
+
+
+
+function limpiarTablaMedico(){
+  $('#tbl-resultado-medico tbody').empty();
+  $('#txt-noidentidad-medico').val("");
+}
+
+function buscarMedico(){
+  var valor = $("#txt-noidentidad-medico").val();
+  $.ajax({
+    url:CONST_SITIO_URL+'/services/Medico.php',
+    method:'POST',
+    dataType:'JSON',
+    data:{
+      'accion':'buscarPorNoIdentidad',
+      'noIdentidad': $("#txt-noidentidad-medico").val(),
+    },
+    success:function(respuesta){
+      console.log(respuesta);
+      agregarFilaTablaBusquedaMedico(respuesta);
+      medico=respuesta;
+    },
+    error: function(error){
+      console.log(error);
+    },
+    complete: function(){
+      //TO-DO
+    }
+  });
+}
+
+function agregarFilaTablaBusquedaMedico(respuesta){
+  $("#tbl-resultado-medico tbody").empty();
+  for (var i = 0; i < respuesta.length; i++) {
+    var medico = respuesta[i];
+    var fila = 
+    "<tr>"+
+    "  <td>"+medico.NO_COLEGIACION+"</td>"+
+    "  <td>"+medico.P_NOMBRE+" "+medico.S_NOMBRE+" "+medico.P_APELLIDO+" "+medico.S_APELLIDO+"</td>"+
+    "  <td>"+medico.NO_IDENTIDAD+"</td>"+
+    '  <td>'+
+        '<button onclick="seleccionarMedico('+i+')" class="btn btn-default btn-md">'+
+          '<span class="glyphicon glyphicon-ok"></span> &nbsp; Seleccionar'+
+        '</button>'+
+      "</td>"+
+    "</tr>";
+    $("#tbl-resultado-medico tbody").append(fila);
+  }
+}
+
+function seleccionarMedico(i){
+	limpiarTablaMedico();
+	var m = medico[i];
+	$("#txt-id-medico").val(m.ID_MEDICO);
+}
+
+function registrar(){
+	var idCentroMedico= $("#txt-id-centroMedico").val();
+	var idMedico= $("#txt-id-medico").val();
+	var idAtencion= $("#txt-atencion").val();
+	var idExpediente= $("#txt-id-expediente").val();
+	var obs = $("#txt-observacion").val();
+	var fecha_hora = parseFecha(fecha, hora); // Funcion ubicada en el archivo config.js
+	
+	$.ajax({
+	  url:CONST_SITIO_URL+'/services/Emergencia.php',
+	  method:'POST',
+	  dataType:'JSON',
+	  data:{
+		 'accion':'crear',
+	    'idCentroMedico': null,
+	    'idMedico': null,
+	    'idAtencion': null,
+	    'idExpediente': null,
+	    'observacion': null,
+	    'fechaHoraAtencion': null,
+	  },
+	  success:function(respuesta){
+	    alert(respuesta.mensaje);
+	  },
+	  error: function(error){
+	    console.log(error);
+	  },
+	  complete: function(){
+	    //TO-DO
+	  }
+	});	
+}
