2447ed8c936118044b5999e34b9b887f16c8e774
Androimeda <mnunezo@unah.hn>
2018-05-01 15:44:51 -0600

    ppantallas/atencionPH

--- FrontEnd/agregar-atencion.php
+++ FrontEnd/agregar-atencion.php
@@ -14,7 +14,98 @@
     <!-- Fixed navbar -->
     <?php include("nav-bar.php") ?>
     <!-- Fin Navbar -->
-    <div class="container">      
+    <div class="container">   
+        <div class="row well card">
+            <div class="col-lg-12" id="titulo">
+                <h1><?php echo $user["CENTRO_MEDICO"] ?></h1>
+                <h1>Registro de Atenciones Pre-Hospitalarias (Ingreso)</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="col-lg-12 well card">
+                    <h4>Buscar Paciente</h4>
+                    <table class="table" id="tbl-busqueda">
+                        <tr>
+                            <td>No de indentidad</td>
+                            <td><input type="text" onkeyup="buscar()" placeholder="Ingresar" class="form-control" id="txt-noidentidad" ></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <button type="button" class="btn btn-default" onclick="limpiar()">Limpiar</button>
+                                <button type="button" class="btn btn-primary" onclick="buscar()">Buscar</button>
+                            </td>
+                        </tr>
+                    </table>
+
+                    <table class="table" id="tbl-resultado">
+                        <thead>
+                            <th>Expediente</th>
+                            <th>Nombre</th>
+                            <th>No Identidad</th>
+                            <th>Seleccionar</th>
+                        </thead>
+                        <tbody></tbody>
+                    </table>
+                </div>
+                <div class="col-lg-12 well card">
+                  <h4>Buscar Paramédico</h4>
+                  <table class="table" id="tbl-busqueda-paramedico">
+                    <tr>
+                      <td>No de identidad</td>
+                      <td><input type="text" placeholder="Ingresar" class="form-control" id="txt-noidentidad-paramedico"></td>
+                    </tr>
+                    <tr>
+                      <td colspan="2">
+                        <button type="button" class="btn btn-default" onclick="limpiarTablaParamedico()">Limpiar</button>
+                        <button type="button" class="btn btn-primary" onclick="buscarParamedico()">Buscar</button>
+                      </td>
+                    </tr>
+                  </table>
+
+                  <table class="table" id="tbl-resultado-paramedico">
+                    <thead>
+                      <th>Licencia</th>
+                      <th>Nombre</th>
+                      <th>No Identidad</th>
+                      <th>Seleccionar</th>
+                    </thead>
+                    <tbody></tbody>
+                  </table>
+                </div>
+            </div>
+            <div class="col-lg-6 well card">
+                <h4>Datos de Ingreso:</h4>
+                <table class="table">
+                    <input type="hidden" id="txt-id-centro-medico" name="" value="<?php echo $user['ID_CENTRO_MEDICO'] ?>">
+                    <tr>
+                        <td>Expediente</td>
+                        <td colspan="2"><input type="text" disabled="disabled" class="form-control" id="txt-id-expediente"></td>
+                    </tr>
+                    <tr>
+                      <td>Paramedico (Identificador)</td>
+                      <td colspan="2"><input type="text" disabled="disabled" class="form-control" id="txt-id-paramedico"></td>
+                    </tr>
+                    <tr>
+                        <td>Observación:</td>
+                        <td colspan="2">
+                            <textarea id="txt-observacion" class="form-control"></textarea>
+                        </td>
+                    </tr>
+                    <tr></tr>
+                    <tr>
+                        <td>Ambulancia:</td>
+                        <td colspan="2">
+                            <input type="text" id="txt-ambulancia" class="form-control">
+                        </td>
+                    </tr>
+                     <td colspan="3">
+                            <button type="button" class="btn btn-primary btn-block" onclick="registrar()">Registrar</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>   
     </div> <!-- /container -->
     <script src="./js/jquery.min.js"></script>
     <script src="./js/bootstrap.min.js"></script>
--- FrontEnd/controlador/agregar-atencion.js
+++ FrontEnd/controlador/agregar-atencion.js
@@ -1,1 +1,143 @@
 $("#nav-item-emergencia").addClass("active")
+
+var paciente = null;
+var paramedico = null;
+
+function limpiar(){
+  $('#tbl-resultado tbody').empty();
+  $('#txt-noidentidad').val("");
+}
+
+function buscar(){
+  var valor = $("#txt-noidentidad").val();
+  $.ajax({
+    url:CONST_SITIO_URL+'/services/Paciente.php',
+    method:'POST',
+    dataType:'JSON',
+    data:{
+      'accion':'buscarPorNoIdentidad',
+      'noIdentidad': valor,
+    },
+    success:function(respuesta){
+      agregarFilaTablaBusqueda(respuesta);
+      paciente = respuesta;
+    },
+    error: function(error){
+      console.log(error);
+    },
+    complete: function(){
+      //TO-DO
+    }
+  });
+}
+
+function agregarFilaTablaBusqueda(respuesta){
+  $("#tbl-resultado tbody").empty();
+  for (var i = 0; i < respuesta.length; i++) {
+    var paciente = respuesta[i];
+    var fila = 
+    "<tr>"+
+    "  <td>"+paciente.ID_EXPEDIENTE+"</td>"+
+    "  <td>"+paciente.P_NOMBRE+" "+paciente.S_NOMBRE+" "+paciente.P_APELLIDO+" "+paciente.S_APELLIDO+"</td>"+
+    "  <td>"+paciente.NO_IDENTIDAD+"</td>"+
+    '  <td>'+
+        '<button onclick="seleccionar('+i+')" class="btn btn-default btn-md">'+
+          '<span class="glyphicon glyphicon-ok"></span> &nbsp; Seleccionar'+
+        '</button>'+
+      "</td>"+
+    "</tr>";
+    $("#tbl-resultado tbody").append(fila);
+  }
+}
+
+function seleccionar(i){
+	limpiar();
+	var p = paciente[i];
+	$("#txt-id-expediente").val(p.ID_EXPEDIENTE);
+}
+
+
+
+function limpiarTablaParamedico(){
+  $('#tbl-resultado-paramedico tbody').empty();
+  $('#txt-noidentidad-paramedico').val("");
+}
+
+function buscarParamedico(){
+  var valor = $("#txt-noidentidad-paramedico").val();
+  $.ajax({
+    url:CONST_SITIO_URL+'/services/Paramedico.php',
+    method:'POST',
+    dataType:'JSON',
+    data:{
+      'accion':'buscarPorNoIdentidad',
+      'noIdentidad': $("#txt-noidentidad-paramedico").val(),
+    },
+    success:function(respuesta){
+      console.log(respuesta);
+      agregarFilaTablaBusquedaParamedico(respuesta);
+      paramedico=respuesta;
+    },
+    error: function(error){
+      console.log(error);
+    },
+    complete: function(){
+      //TO-DO
+    }
+  });
+}
+
+function agregarFilaTablaBusquedaParamedico(respuesta){
+  $("#tbl-resultado-paramedico tbody").empty();
+  for (var i = 0; i < respuesta.length; i++) {
+    var paramedico = respuesta[i];
+    var fila = 
+    "<tr>"+
+    "  <td>"+paramedico.LICENCIA+"</td>"+
+    "  <td>"+paramedico.P_NOMBRE+" "+paramedico.S_NOMBRE+" "+paramedico.P_APELLIDO+" "+paramedico.S_APELLIDO+"</td>"+
+    "  <td>"+paramedico.NO_IDENTIDAD+"</td>"+
+    '  <td>'+
+        '<button onclick="seleccionarparaMedico('+i+')" class="btn btn-default btn-md">'+
+          '<span class="glyphicon glyphicon-ok"></span> &nbsp; Seleccionar'+
+        '</button>'+
+      "</td>"+
+    "</tr>";
+    $("#tbl-resultado-paramedico tbody").append(fila);
+  }
+}
+
+function seleccionarParamedico(i){
+	limpiarTablaParamedico();
+	var m = paramedico[i];
+	$("#txt-id-paramedico").val(m.ID_PARAMEDICO);
+}
+
+function registrar(){
+	var obs = $("#txt-observacion").val();
+	var idAmbulancia = $("#txt-ambulancia").val();
+	var idParamedico= $("#txt-id-paramedico").val();
+	var idExpediente= $("#txt-id-expediente").val();
+ 
+	$.ajax({
+	  url:CONST_SITIO_URL+'/services/AtencionPreHospitalaria.php',
+	  method:'POST',
+	  dataType:'JSON',
+	  data:{
+		'accion':'crear',
+	    'observacion': obs,
+	    'idAmbulancia': idAmbulancia,
+	    'idParamedico': idParamedico,
+	    'idExpediente': idExpediente,
+	   
+	  },
+	  success:function(respuesta){
+	    alert(respuesta.mensaje);
+	  },
+	  error: function(error){
+	    console.log(error);
+	  },
+	  complete: function(){
+	    //TO-DO
+	  }
+	});	
+}
