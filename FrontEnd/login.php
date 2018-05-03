497cfae013475f25fe9dcd54e59879ef1f233ae5
Jose Antonio Aguilar <jaguilar992@gmail.com>
2018-05-02 19:37:46 -0600

    Lo de antes

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -127,7 +127,7 @@
                 <div class="modal-content">
                   <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Registrar Paciente</h4>
+                    <h4 class="modal-title" id="myModalLabel">Registrar Centro</h4>
                   </div>
                   <input type="hidden" value="" id="txt-id-paciente">
                   <div class="modal-body">
